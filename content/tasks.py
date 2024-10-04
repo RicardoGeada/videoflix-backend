ec19c901625ff9d2b49e1e42b43d7c1d24799fee
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-10-04 09:52:45 +0200

    refactoring

--- content/tasks.py
+++ content/tasks.py
@@ -1,32 +1,19 @@
 import subprocess
 import os
 
-def convert_480p(source):
-    file_name, _ = os.path.splitext(source)
-    target = file_name + '_480p.mp4'
-    cmd = f'ffmpeg -i "{source}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{target}"'
-    subprocess.run(cmd, shell=True)
-    return target
+def convert_to_hls(source, resolution):
 
-def convert_720p(source):
-    file_name, _ = os.path.splitext(source)
-    target = file_name + '_720p.mp4'
-    cmd = f'ffmpeg -i "{source}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{target}"'
-    subprocess.run(cmd, shell=True)
-    return target
-
-def convert_1080p(source):
-    file_name, _ = os.path.splitext(source)
-    target = file_name + '_1080p.mp4'
-    cmd = f'ffmpeg -i "{source}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{target}"'
-    subprocess.run(cmd, shell=True)
-    return target
+    allowed_resolutions = ['480', '720', '1080']
+    if resolution not in allowed_resolutions:
+        raise ValueError(f"Invalid resolution. Please choose from {allowed_resolutions}")  
     
-    
-def convert_to_hls(source):
     file_name, _ = os.path.splitext(source)
-    hls_target = file_name + '.m3u8'
-    cmd = f'ffmpeg -i "{source}" -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls "{hls_target}"'
+    hls_target = file_name + f'_{resolution}p.m3u8'
+    cmd = (
+        f'ffmpeg -i "{source}" -s hd{resolution} -c:v libx264 -crf 23 -c:a aac '
+        f'-start_number 0 -hls_time 10 -hls_list_size 0 -f hls "{hls_target}"'
+    )
+    
     subprocess.run(cmd, shell=True)
     return hls_target
     
@@ -34,3 +21,5 @@ def convert_to_hls(source):
 def delete_file(file_path):
     if os.path.exists(file_path):
         os.remove(file_path)
+        
+
