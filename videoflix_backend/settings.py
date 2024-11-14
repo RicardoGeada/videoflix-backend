40ed82791d93007e813a0b6271b122f5488855f4
Ricardo Geada <ricardogeada.dev@gmail.com>
2024-11-14 08:21:30 +0000

    update for server

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv('ENV_LIN_HOST'), 'localhost', '127.0.0.1', os.getenv('HOST_URL')]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200', 'https://videoflix.ricardogeada.com']
 
 # Application definition
 
